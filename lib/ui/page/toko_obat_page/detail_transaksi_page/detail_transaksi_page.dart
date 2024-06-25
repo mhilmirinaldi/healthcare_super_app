c550fcae3041726261dcb511579da64e67527c58
Mohamad Hilmi Rinaldi <aldi280402@gmail.com>
2024-06-26 00:28:52 +0700

    edit detail transaksi page

--- lib/ui/page/toko_obat_page/detail_transaksi_page/detail_transaksi_page.dart
+++ lib/ui/page/toko_obat_page/detail_transaksi_page/detail_transaksi_page.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:super_app_telemedicine/domain/entity/transaksi.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:super_app_telemedicine/ui/extension/constant.dart';
+import 'package:super_app_telemedicine/ui/misc/colors.dart';
 import 'package:super_app_telemedicine/ui/provider/router/router_provider.dart';
 
 class DetailTransaksiPage extends ConsumerStatefulWidget {
@@ -19,26 +20,27 @@ class DetailTransaksiPage extends ConsumerStatefulWidget {
 
 class _DetailTransaksiPageState extends ConsumerState<DetailTransaksiPage> {
   final Completer<GoogleMapController> _controller = Completer();
-  static const LatLng _initialPosition = LatLng(-6.21462, 106.84513);
-  static const LatLng _pickupPosition = LatLng(-6.22462, 106.84513);
-  static const LatLng _destinationPosition = LatLng(-6.23462, 106.84513);
+  static const LatLng _initialPosition = LatLng(-6.944683, 107.671837);
+  static const LatLng _pickupPosition = LatLng(-6.947940, 107.676121);
+  static const LatLng _destinationPosition = LatLng(-6.959329, 107.674962);
   List<LatLng> polylineCoordinates = [];
   BitmapDescriptor? _driverIcon;
-  LatLng _driverPosition = _initialPosition;
+  BitmapDescriptor? _pickupIcon;
+  BitmapDescriptor? _destinationIcon;
   int _currentStep = 1;
+  int _polylineIndex = 0;
   late Timer _timer;
 
   @override
   void initState() {
     super.initState();
     _setPolyline();
-    _startSimulation();
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    _setDriverIcon();
+    _setCustomIcons();
   }
 
   @override
@@ -47,15 +49,25 @@ class _DetailTransaksiPageState extends ConsumerState<DetailTransaksiPage> {
     super.dispose();
   }
 
-  void _setDriverIcon() async {
+  void _setCustomIcons() async {
     ImageConfiguration configuration = createLocalImageConfiguration(context);
-    BitmapDescriptor assetIcon = await BitmapDescriptor.asset(
+    BitmapDescriptor driverIcon = await BitmapDescriptor.fromAssetImage(
+      configuration,
+      'assets/alumnus.png',
+    );
+    BitmapDescriptor pickupIcon = await BitmapDescriptor.fromAssetImage(
+      configuration,
+      'assets/nomor_str.png',
+    );
+    BitmapDescriptor destinationIcon = await BitmapDescriptor.fromAssetImage(
       configuration,
       'assets/nomor_str.png',
     );
 
     setState(() {
-      _driverIcon = assetIcon;
+      _driverIcon = driverIcon;
+      _pickupIcon = pickupIcon;
+      _destinationIcon = destinationIcon;
     });
   }
 
@@ -64,7 +76,8 @@ class _DetailTransaksiPageState extends ConsumerState<DetailTransaksiPage> {
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
       api_map_key,
       PointLatLng(_initialPosition.latitude, _initialPosition.longitude),
-      PointLatLng(_destinationPosition.latitude, _destinationPosition.longitude),
+      PointLatLng(
+          _destinationPosition.latitude, _destinationPosition.longitude),
     );
 
     if (result.points.isNotEmpty) {
@@ -72,30 +85,44 @@ class _DetailTransaksiPageState extends ConsumerState<DetailTransaksiPage> {
         polylineCoordinates = result.points
             .map((point) => LatLng(point.latitude, point.longitude))
             .toList();
+        _startSimulation();
       });
     }
   }
 
   void _startSimulation() {
-    _timer = Timer.periodic(const Duration(seconds: 2), (timer) {
-      setState(() {
-        if (_currentStep < 4) {
-          _currentStep++;
-          if (_currentStep == 2) {
-            _driverPosition = _pickupPosition;
-          } else if (_currentStep == 3) {
-            _driverPosition = LatLng(
-                (_pickupPosition.latitude + _destinationPosition.latitude) / 2,
-                (_pickupPosition.longitude + _destinationPosition.longitude) / 2);
-          } else if (_currentStep == 4) {
-            _driverPosition = _destinationPosition;
-          }
-        } else {
-          timer.cancel();
-        }
-      });
+    const updateInterval = Duration(milliseconds: 250);
+    _timer = Timer.periodic(updateInterval, (timer) {
+      if (_polylineIndex < polylineCoordinates.length - 1) {
+        setState(() {
+          _polylineIndex++;
+          _updateDriverStep();
+        });
+      } else {
+        timer.cancel();
+      }
     });
   }
+
+  void _updateDriverStep() {
+    LatLng currentPosition = polylineCoordinates[_polylineIndex];
+    const double marginError = 50;
+
+    if (_distanceBetween(currentPosition, _pickupPosition) < marginError) {
+      setState(() {
+        _currentStep = 2;
+      });
+    } else if (_distanceBetween(currentPosition, _destinationPosition) <
+        marginError) {
+      setState(() {
+        _currentStep = 3;
+      });
+    }
+  }
+
+  LatLng get _driverPosition => _polylineIndex < polylineCoordinates.length
+      ? polylineCoordinates[_polylineIndex]
+      : _destinationPosition;
 
   @override
   Widget build(BuildContext context) {
@@ -105,7 +132,8 @@ class _DetailTransaksiPageState extends ConsumerState<DetailTransaksiPage> {
         child: Container(
           decoration: BoxDecoration(
             border: Border(
-              bottom: BorderSide(color: Colors.grey.withOpacity(0.3), width: 1.0),
+              bottom:
+                  BorderSide(color: Colors.grey.withOpacity(0.3), width: 1.0),
             ),
           ),
           child: AppBar(
@@ -132,81 +160,70 @@ class _DetailTransaksiPageState extends ConsumerState<DetailTransaksiPage> {
           ),
         ),
       ),
-      body: ListView(
+      body: Column(
         children: [
           SizedBox(
-            height: 300,
+            height: 400,
             child: GoogleMap(
               initialCameraPosition: const CameraPosition(
-                target: _initialPosition,
+                target: _destinationPosition,
                 zoom: 14.0,
               ),
               polylines: {
                 Polyline(
                   polylineId: const PolylineId('route'),
                   points: polylineCoordinates,
-                  color: Colors.blue,
-                  width: 6,
+                  color: primaryColor,
+                  width: 5,
                 ),
               },
-              markers: _driverIcon == null ? Set() : {
-                Marker(
-                  markerId: const MarkerId('driver'),
-                  position: _driverPosition,
-                  icon: _driverIcon!,
-                ),
+              markers: {
+                if (_driverIcon != null)
+                  Marker(
+                    markerId: const MarkerId('driver'),
+                    position: _driverPosition,
+                    icon: _driverIcon!,
+                  ),
+                if (_pickupIcon != null)
+                  Marker(
+                    markerId: const MarkerId('pickup'),
+                    position: _pickupPosition,
+                    icon: _pickupIcon!,
+                  ),
+                if (_destinationIcon != null)
+                  Marker(
+                    markerId: const MarkerId('destination'),
+                    position: _destinationPosition,
+                    icon: _destinationIcon!,
+                  ),
               },
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
               },
+              myLocationEnabled: true,
+              compassEnabled: true,
+              zoomControlsEnabled: true,
+              zoomGesturesEnabled: true,
+              scrollGesturesEnabled: true,
+              tiltGesturesEnabled: true,
+              rotateGesturesEnabled: true,
             ),
           ),
-          Stepper(
-            currentStep: _currentStep - 1,
-            steps: [
-              Step(
-                title: const Text('Pesanan Terkonfirmasi'),
-                content: const Text('Pesanan Anda telah dikonfirmasi.'),
-                isActive: _currentStep >= 1,
-              ),
-              Step(
-                title: const Text('Driver Ditugaskan'),
-                content: const Text('Driver telah ditugaskan untuk pesanan Anda.'),
-                isActive: _currentStep >= 2,
-              ),
-              Step(
-                title: const Text('Dalam Perjalanan'),
-                content: const Text('Driver sedang dalam perjalanan.'),
-                isActive: _currentStep >= 3,
-              ),
-              Step(
-                title: const Text('Pesanan Diterima'),
-                content: const Text('Pesanan telah diterima.'),
-                isActive: _currentStep >= 4,
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(16.0),
               children: [
-                const Text(
-                  'Pesanan',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _buildStepIndicator(1, "Pesanan \nTerkonfirmasi"),
+                    _buildStepIndicator(2, "Driver \nDitugaskan"),
+                    _buildStepIndicator(3, "Dalam \nPerjalanan"),
+                    _buildStepIndicator(4, "Pesanan \nDiterima"),
+                  ],
                 ),
-                const SizedBox(height: 8),
-                Text(widget.transaksi.toString()),
                 const SizedBox(height: 16),
-                const Text(
-                  'Pembayaran',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                const SizedBox(height: 8),
-                const Text('Barang: Rp10.000'),
-                const Text('Biaya Pengiriman: Rp9.000'),
-                const Text('Biaya Layanan: Rp5.000'),
-                const Text('Jumlah Pembayaran: Rp24.000'),
+                _buildOrderDetails(),
               ],
             ),
           ),
@@ -214,4 +231,55 @@ class _DetailTransaksiPageState extends ConsumerState<DetailTransaksiPage> {
       ),
     );
   }
+
+  Widget _buildStepIndicator(int step, String title) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 20,
+          backgroundColor: _currentStep >= step ? Colors.green : Colors.grey,
+          child: Text(
+            step.toString(),
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+        const SizedBox(height: 4),
+        Text(title,
+            textAlign: TextAlign.center, style: const TextStyle(fontSize: 12)),
+      ],
+    );
+  }
+
+  Widget _buildOrderDetails() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Pesanan',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        const SizedBox(height: 8),
+        Text(widget.transaksi.toString()),
+        const SizedBox(height: 16),
+        const Text(
+          'Pembayaran',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        const SizedBox(height: 8),
+        const Text('Barang: Rp10.000'),
+        const Text('Biaya Pengiriman: Rp9.000'),
+        const Text('Biaya Layanan: Rp5.000'),
+        const Text('Jumlah Pembayaran: Rp24.000'),
+      ],
+    );
+  }
+}
+
+double _distanceBetween(LatLng start, LatLng end) {
+  return Geolocator.distanceBetween(
+    start.latitude,
+    start.longitude,
+    end.latitude,
+    end.longitude,
+  );
 }
